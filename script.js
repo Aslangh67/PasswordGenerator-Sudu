2f7280c443b816ae4fb5e6faaa7982424188562a
Mr. A <ag.perch@gmail.com>
2020-09-10 15:20:18 -0700

    added the sodu coding for the homework

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ function writePassword() {
 
 }
 
+
 // TODO: Create the generatePassword function that returns the final password and all of my code should live inside this unction
 
-// TODO: Creat a prompt to ask the length of the password and save it to a VAR
+// TODO: Create a prompt to ask the length of the password and save it to a VAR
 
 // TODO: Create a Confirm to ask the user if they want lowercase char
 
@@ -23,6 +24,23 @@ function writePassword() {
 // TODO: Create a Confirm to ask the user if they want special char
 
 
+// TODO: After I'm done with the prompts I have to Create the password
+
+// TODO: Create arrys with all needed char (an array for lowercase char, one for uppercase char etc)
+
+// TODO: create a new var that holds all the chosen char
+
+// TODO: create a Conditional situation that saves the choisen char from our confirms to our chosen char var (using an if statement)
+
+// TODO: We have to select the specific number of char randomly from our final collection based on the user's answer to how long they want the length of their pass to be
+
+// I can use Math.floor(Math.random()) to get a random number
+// for loop that runs as long as the prompt to the lenght answer [ for(var i=0 ; i<"charactor length prompt" ; i++ ){"logic goes here"}]
+
+
+// TODO: Add validations to make sure atleast one type of char is selected and alert the user if there are no types being selected
+
+// TODO: Add validation to the length to make sure it is between 8-128 char
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
